9f86574c1be0d1b70badfe2ebef48d139f56ac1b
sixthson6 <sudais2408@gmail.com>
2025-06-11 09:42:19 -0700

    Add User service layer

---
+++ src/main/java/com/tech/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tech.security.service;
+
+import com.tech.security.repository.UserRepository;
+import com.tech.security.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+    }
+}
