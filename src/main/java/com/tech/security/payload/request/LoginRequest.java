20036e60dccc94705146131a8c8428c2938516af
sixthson6 <sudais2408@gmail.com>
2025-06-11 09:43:29 -0700

    Add response and request DTOs

---
+++ src/main/java/com/tech/security/payload/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.tech.security.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
---
+++ src/main/java/com/tech/security/payload/request/SignupRequest.java
@@ -0,0 +1,26 @@
+package com.tech.security.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequest {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Size(max = 50, message = "Email cannot exceed 50 characters")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
+    private String password;
+
+    private Set<String> roles; // (e.g., "admin", "developer")
+}
