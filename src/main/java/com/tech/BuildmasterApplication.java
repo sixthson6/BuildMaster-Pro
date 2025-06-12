797a5c47701e8823fcbe1176e199cef6fa117005
sixthson6 <sudais2408@gmail.com>
2025-06-12 09:33:25 +0000

    Revert changes to main class

--- src/main/java/com/tech/BuildmasterApplication.java
+++ src/main/java/com/tech/BuildmasterApplication.java
@@ -1,8 +1,14 @@
 package com.tech;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -11,5 +17,4 @@ public class BuildmasterApplication {
     public static void main(String[] args) {
         SpringApplication.run(BuildmasterApplication.class, args);
     }
-
 }
