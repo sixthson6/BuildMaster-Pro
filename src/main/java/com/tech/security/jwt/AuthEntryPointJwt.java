1e8584158054b815e9c977e6d3df6d17be0668fc
sixthson6 <sudais2408@gmail.com>
2025-06-16 09:36:29 +0000

    Remove unwanted comments

--- src/main/java/com/tech/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/tech/security/jwt/AuthEntryPointJwt.java
@@ -20,7 +20,6 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
         logger.error("Unauthorized error: {}", authException.getMessage());
-        // HttpServletResponse.SC_UNAUTHORIZED is 401
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
 }
